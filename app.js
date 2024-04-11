919bb287076914f67de97a1f95d28164607465fe
Lakshit Jain <jainlakshit849@gmail.com>
2024-04-11 15:53:59 +0530

    Implememnted Show Listing Route

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require("path");
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const port = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/airbnb";
 main()
@@ -41,6 +43,13 @@ async function main() {
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
+});
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
 });
 
 app.listen(port, () => {
