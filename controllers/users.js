6824e520e1524e0fdc050c6870a6e4859ac8524b
Lakshit Jain <jainlakshit849@gmail.com>
2024-05-29 12:00:23 +0530

    MVC Controller for Users

---
+++ controllers/users.js
@@ -0,0 +1,43 @@
+const User = require("../models/user.js");
+
+module.exports.renderSignUpForm = (req, res) => {
+  res.render("users/signup.ejs");
+};
+
+module.exports.signup = async (req, res) => {
+  try {
+    let { username, email, password } = req.body;
+    let newUser = new User({ email, username });
+    let registeredUser = await User.register(newUser, password);
+    req.login(registeredUser, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", "Welcome to Airbnb!");
+      res.redirect("/listings");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
+};
+
+module.exports.renderLoginForm = (req, res) => {
+  res.render("users/login.ejs");
+};
+
+module.exports.login = async (req, res) => {
+  req.flash("success", "Welcome to AirBnb! You are logged in.");
+  let redirectUrl = res.locals.redirectUrl || "/listings";
+  res.redirect(redirectUrl);
+};
+
+module.exports.logout = (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      next(err);
+    }
+    req.flash("success", "Logged Out Successfully !");
+    res.redirect("/listings");
+  });
+};
--- routes/user.js
+++ routes/user.js
@@ -1,38 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
+const userController = require("../controllers/users.js");
 
-router.get("/signup", (req, res) => {
-  res.render("users/signup.ejs");
-});
+router.get("/signup", userController.renderSignUpForm);
 
-router.post(
-  "/signup",
-  wrapAsync(async (req, res) => {
-    try {
-      let { username, email, password } = req.body;
-      let newUser = new User({ email, username });
-      let registeredUser = await User.register(newUser, password);
-      req.login(registeredUser, (err) => {
-        if (err) {
-          return next(err);
-        }
-        req.flash("success", "Welcome to Airbnb!");
-        res.redirect("/listings");
-      });
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/signup");
-    }
-  })
-);
+router.post("/signup", wrapAsync(userController.signup));
 
-router.get("/login", (req, res) => {
-  res.render("users/login.ejs");
-});
+router.get("/login", userController.renderLoginForm);
 
 router.post(
   "/login",
@@ -41,21 +18,9 @@ router.post(
     failureRedirect: "/login",
     failureFlash: true,
   }),
-  wrapAsync(async (req, res) => {
-    req.flash("success", "Welcome to AirBnb! You are logged in.");
-    let redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
-  })
+  wrapAsync(userController.login)
 );
 
-router.get("/logout", (req, res) => {
-  req.logout((err) => {
-    if (err) {
-      next(err);
-    }
-    req.flash("success", "Logged Out Successfully !");
-    res.redirect("/listings");
-  });
-});
+router.get("/logout", userController.logout);
 
 module.exports = router;
