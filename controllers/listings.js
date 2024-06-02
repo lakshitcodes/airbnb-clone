8104987162fe93ea8360543dd41c6f6593246424
Lakshit Jain <jainlakshit849@gmail.com>
2024-06-02 10:08:09 +0530

    Added Map Functionality

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,7 @@
 const Listing = require("../models/listing.js");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
@@ -28,12 +31,19 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
+  let response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1,
+    })
+    .send();
   let url = req.file.path;
   let filename = req.file.filename;
   let listing = new Listing(req.body.listing);
   listing.owner = req.user._id;
   listing.image = { url, filename };
-  await listing.save();
+  listing.geometry = response.body.features[0].geometry;
+  let savedListing = await listing.save();
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
 };
